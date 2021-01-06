47bc91b3c043837d6ffbc7ebc3cfc97c6abe5b1b
sossam <heart_b_e@naver.com>
2021-01-06 18:07:52 +0900

    ✨ drawDiagram 함수 추가

--- SwiftTree/main.swift
+++ SwiftTree/main.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 
-class Node {
-    var data: Int
+class Node<T: Comparable> {
+    var data: T
     var left: Node?
     var right: Node?
     
-    init(data: Int) {
+    init(data: T) {
         self.data = data
     }
 }
 
 
-class BinarySearchTree {
-    var root: Node?
+class BinarySearchTree<T: Comparable> {
+    var root: Node<T>?
     
-    func insert(_ data: Int) {
+    func insert(_ data: T) {
         guard let root = self.root else {
             return self.root = Node.init(data: data)
         }
@@ -43,7 +43,7 @@ class BinarySearchTree {
         }
     }
     
-    func search(from data: Int) -> Bool {
+    func search(from data: T) -> Bool {
         if root == nil { return false }
         
         var currentNode = root
@@ -59,9 +59,28 @@ class BinarySearchTree {
         }
         return false
     }
+    
+    func drawDiagram() {
+        print(diagram(for: self.root))
+    }
+
+    private func diagram(for node: Node<T>?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+        guard let node = node else {
+            return root + "nil\n"
+        }
+        if node.left == nil && node.right == nil {
+            return root + "\(node.data)\n"
+        }
+        return diagram(for: node.right, top + " ", top + "┌──", top + "│ ")
+            + root + "\(node.data)\n"
+            + diagram(for: node.left, bottom + "│ ", bottom + "└──", bottom + " ")
+    }
 }
 
-let BST = BinarySearchTree.init()
+let BST = BinarySearchTree<Int>()
 BST.insert(20)
 BST.insert(15)
 BST.insert(30)
@@ -70,6 +89,7 @@ BST.insert(16)
 BST.insert(35)
 BST.insert(37)
 
+BST.drawDiagram()
 
 print(BST.search(from: 20))
 print(BST.search(from: 15))
@@ -77,4 +97,5 @@ print(BST.search(from: 12))
 print(BST.search(from: 60))
 print(BST.search(from: 0))
 print(BST.search(from: 100))
+
 
